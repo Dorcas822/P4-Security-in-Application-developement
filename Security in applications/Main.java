bdfca0eb8391cce11657390064b414581b9cb5af
Dorcas822 <sibonginkosinyathi84@gmail.com>
2025-04-22 23:48:03 +0200

    Add files via upload

---
+++ Security in applications/Main.java
@@ -0,0 +1,6 @@
+import java.sql.*;
+
+// JDBC connection variables
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/Data?useSSL=false";
+    static final String DB_USER = "Admin";
+    static final String DB_PASSWORD = "H45H";
